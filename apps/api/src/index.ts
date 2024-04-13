dc021297186154570189f92ec66c8f624806a3ad
hmartiins <hmartins224@gmail.com>
2024-04-13 13:08:00 -0300

    feat: define app permissions

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,12 +1,6 @@
 import { defineAbilitiesFor } from '@saas-rbac/auth'
 
-const ability = defineAbilitiesFor({ role: 'MEMBER' })
+const ability = defineAbilitiesFor({ role: 'MEMBER', id: '1' })
 
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
-const userCanDeleteOtherUsers = ability.can('delete', 'User')
-
-const userCannotDeleteOtherUsers = ability.cannot('delete', 'User')
-
-console.log(userCanInviteSomeoneElse)
-console.log(userCanDeleteOtherUsers)
-console.log(userCannotDeleteOtherUsers)
+console.log(ability.can('get', 'Billing'))
+console.log(ability.can('create', 'Invite'))
