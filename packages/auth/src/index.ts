0b33f1c5455e6f7f9ffd6c44303a816a6e7c8bf8
hmartiins <hmartins224@gmail.com>
2024-04-13 13:11:28 -0300

    refactor: organize application imports

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -6,17 +6,17 @@ import {
 } from '@casl/ability'
 import { z } from 'zod'
 
-import { User } from './models/user'
+import { User } from './models'
 import { permissions } from './permissions'
-import { billingSubject } from './subjects/billing'
-import { inviteSubject } from './subjects/invite'
-import { organizationSubject } from './subjects/organization'
-import { projectSubject } from './subjects/project'
-import { userSubject } from './subjects/user'
+import {
+  billingSubject,
+  inviteSubject,
+  organizationSubject,
+  projectSubject,
+  userSubject,
+} from './subjects'
 
-export * from './models/organization'
-export * from './models/project'
-export * from './models/user'
+export * from './models'
 
 const appAbilities = z.union([
   projectSubject,
