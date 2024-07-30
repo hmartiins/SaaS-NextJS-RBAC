8108bcffb5f76d5af805c1d84edab266be09cc31
hmartiins <hmartins224@gmail.com>
2024-07-29 21:32:01 -0300

    feat: setup Fastify Swagger and Swagger UI for API documentation

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -1,6 +1,9 @@
 import fastifyCors from '@fastify/cors'
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUI from '@fastify/swagger-ui'
 import { fastify } from 'fastify'
 import {
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
@@ -12,6 +15,22 @@ const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setSerializerCompiler(serializerCompiler)
 app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'NextJS Saas RBAC',
+      description: 'Full-Stack SaaS app with multi-tenant, RBAC, and more',
+      version: '1.0.0',
+    },
+    servers: [],
+  },
+  transform: jsonSchemaTransform,
+})
+
+app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+})
 
 app.register(fastifyCors)
 
