1d4d7a0c6ef50feb3920c2863ff75b46762d667d
hmartiins <hmartins224@gmail.com>
2024-08-07 21:49:24 -0300

    feat: create getUserMembership middleware method

--- apps/api/src/http/middlewares/auth.ts
+++ apps/api/src/http/middlewares/auth.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from 'fastify'
 import fastifyPlugin from 'fastify-plugin'
 
+import { prisma } from '@/lib/prisma'
+
 import { UnauthorizedRequestError } from '../routes/_errors'
 
 export const auth = fastifyPlugin(async function (app: FastifyInstance) {
@@ -13,6 +15,31 @@ export const auth = fastifyPlugin(async function (app: FastifyInstance) {
       } catch {
         throw new UnauthorizedRequestError('Invalid auth token')
       }
+    }
+
+    request.getUserMembership = async (slug: string) => {
+      const userId = await request.getCurrentUserId()
+      const member = await prisma.member.findFirst({
+        where: {
+          userId,
+          organization: {
+            slug,
+          },
+        },
+        include: {
+          organization: true,
+        },
+      })
+
+      if (!member) {
+        throw new UnauthorizedRequestError(
+          'You`re not a member of this organization',
+        )
+      }
+
+      const { organization, ...membership } = member
+
+      return { organization, membership }
     }
   })
 })
