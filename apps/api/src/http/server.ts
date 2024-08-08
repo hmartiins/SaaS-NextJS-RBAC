a0ea0a9d9b942dbf0ff3b3f1a0d421c8f5390e11
hmartiins <hmartins224@gmail.com>
2024-08-07 22:17:59 -0300

    feat: Add routes for retrieving organization details and organizations list

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -18,6 +18,8 @@ import { requestPasswordRecover } from './routes/auth/request-password-recovery'
 import { resetPassword } from './routes/auth/reset-password'
 import { createOrganization } from './routes/orgs/create-organization'
 import { getMembership } from './routes/orgs/get-membership'
+import { getOrganization } from './routes/orgs/get-organization'
+import { getOrganizations } from './routes/orgs/get-organizations'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -66,6 +68,8 @@ app.register(authenticateWithGithub)
 
 app.register(createOrganization)
 app.register(getMembership)
+app.register(getOrganization)
+app.register(getOrganizations)
 
 app.listen({ port: env.SERVER_PORT }, () => {
   console.log('Server is running 🚀')
