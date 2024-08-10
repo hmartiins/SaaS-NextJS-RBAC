677831cff52ef0894682cf4ed33830de9d65620a
hmartiins <hmartins224@gmail.com>
2024-08-09 22:55:39 -0300

    feat: Add createProject route for creating projects

--- apps/api/src/http/server.ts
+++ apps/api/src/http/server.ts
@@ -23,6 +23,7 @@ import { getOrganizations } from './routes/orgs/get-organizations'
 import { shutdownOrganization } from './routes/orgs/shutdown-organization'
 import { transferOrganization } from './routes/orgs/transfer-organization'
 import { updateOrganization } from './routes/orgs/update-organization'
+import { createProject } from './routes/projects/create-project'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -76,6 +77,8 @@ app.register(getOrganizations)
 app.register(updateOrganization)
 app.register(shutdownOrganization)
 app.register(transferOrganization)
+
+app.register(createProject)
 
 app.listen({ port: env.SERVER_PORT }, () => {
   console.log('Server is running 🚀')
