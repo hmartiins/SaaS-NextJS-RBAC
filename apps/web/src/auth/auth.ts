d98ba4c5e833df1c4f3e34f13e98ef666b37274d
hmartiins <hmartins224@gmail.com>
2024-08-17 14:07:53 -0300

    feat(web): get user permissions

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,10 +1,43 @@
+import { defineAbilityFor } from '@saas-rbac/auth'
 import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
+import { getMembership } from '@/http/get-membership'
 import { getProfile } from '@/http/get-profile'
 
 export function isAuthenticated() {
   return !!cookies().get('token')?.value
+}
+
+export function getCurrentOrg() {
+  return cookies().get('org')?.value ?? null
+}
+
+export async function getCurrentMembership() {
+  const currentOrg = getCurrentOrg()
+
+  if (!currentOrg) {
+    return null
+  }
+
+  const { membership } = await getMembership(currentOrg)
+
+  return membership
+}
+
+export async function ability() {
+  const membership = await getCurrentMembership()
+
+  if (!membership) {
+    return null
+  }
+
+  const ability = defineAbilityFor({
+    id: membership.userId,
+    role: membership.role,
+  })
+
+  return ability
 }
 
 export async function auth() {
