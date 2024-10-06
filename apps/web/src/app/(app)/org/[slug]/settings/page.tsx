4773a26f6e7f17c6a5febca028dd23b4b29a55af
hmartiins <hmartins224@gmail.com>
2024-10-06 20:08:02 -0300

    feat: add billing functionality to organization settings page

--- apps/web/src/app/(app)/org/[slug]/settings/page.tsx
+++ apps/web/src/app/(app)/org/[slug]/settings/page.tsx
@@ -9,6 +9,7 @@ import {
 import { getOrganization } from '@/http/get-organization'
 
 import { OrganizationForm } from '../../organization-form'
+import { Billing } from './billing'
 import { ShutdownOrganizationButton } from './shutdown-organization-button'
 
 export default async function Default() {
@@ -49,7 +50,7 @@ export default async function Default() {
           </Card>
         )}
 
-        {canGetBilling && <div>billing</div>}
+        {canGetBilling && <Billing />}
 
         {canShutdownOrganization && (
           <Card>
