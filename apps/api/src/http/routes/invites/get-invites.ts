9a01d06ab42a687ae716586485ce24494085b72b
hmartiins <hmartins224@gmail.com>
2024-10-06 21:43:44 -0300

    feat: create revoke invite function

--- apps/api/src/http/routes/invites/get-invites.ts
+++ apps/api/src/http/routes/invites/get-invites.ts
@@ -3,77 +3,82 @@ import { FastifyInstance } from 'fastify'
 import { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
+import { auth } from '@/http/middlewares/auth'
 import { prisma } from '@/lib/prisma'
 import { getUserPermissions } from '@/utils'
 
 import { UnauthorizedRequestError } from '../_errors'
 
 export async function getInvites(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().get(
-    '/organizations/:slug/invites',
-    {
-      schema: {
-        tags: ['invites'],
-        summary: 'Get all invites of an organization',
-        params: z.object({
-          slug: z.string(),
-        }),
-        response: {
-          200: z.object({
-            invites: z.array(
-              z.object({
-                id: z.string(),
-                createdAt: z.date(),
-                role: roleSchema,
-                email: z.string().email(),
-                author: z
-                  .object({
-                    id: z.string().uuid(),
-                    name: z.string().nullable(),
-                  })
-                  .nullable(),
-              }),
-            ),
+  app
+    .withTypeProvider<ZodTypeProvider>()
+    .register(auth)
+    .get(
+      '/organizations/:slug/invites',
+      {
+        schema: {
+          tags: ['invites'],
+          summary: 'Get all invites of an organization',
+          params: z.object({
+            slug: z.string(),
           }),
-        },
-      },
-    },
-    async (request) => {
-      const userId = await request.getCurrentUserId()
-
-      const { slug } = request.params
-      const { membership, organization } = await request.getUserMembership(slug)
-
-      const { cannot } = getUserPermissions(userId, membership.role)
-
-      if (cannot('get', 'Invite')) {
-        throw new UnauthorizedRequestError(
-          'You are not allowed to get organization invites.',
-        )
-      }
-
-      const invites = await prisma.invite.findMany({
-        where: {
-          organizationId: organization.id,
-        },
-        select: {
-          id: true,
-          email: true,
-          role: true,
-          createdAt: true,
-          author: {
-            select: {
-              id: true,
-              name: true,
-            },
+          response: {
+            200: z.object({
+              invites: z.array(
+                z.object({
+                  id: z.string(),
+                  createdAt: z.date(),
+                  role: roleSchema,
+                  email: z.string().email(),
+                  author: z
+                    .object({
+                      id: z.string().uuid(),
+                      name: z.string().nullable(),
+                    })
+                    .nullable(),
+                }),
+              ),
+            }),
           },
         },
-        orderBy: {
-          createdAt: 'desc',
-        },
-      })
+      },
+      async (request) => {
+        const userId = await request.getCurrentUserId()
 
-      return { invites }
-    },
-  )
+        const { slug } = request.params
+        const { membership, organization } =
+          await request.getUserMembership(slug)
+
+        const { cannot } = getUserPermissions(userId, membership.role)
+
+        if (cannot('get', 'Invite')) {
+          throw new UnauthorizedRequestError(
+            'You are not allowed to get organization invites.',
+          )
+        }
+
+        const invites = await prisma.invite.findMany({
+          where: {
+            organizationId: organization.id,
+          },
+          select: {
+            id: true,
+            email: true,
+            role: true,
+            createdAt: true,
+            author: {
+              select: {
+                id: true,
+                name: true,
+              },
+            },
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        })
+
+        return { invites }
+      },
+    )
 }
--- apps/web/src/app/(app)/org/[slug]/members/invites.tsx
+++ apps/web/src/app/(app)/org/[slug]/members/invites.tsx
@@ -1,3 +1,60 @@
-export default function Invites() {
-  return <div>invites</div>
+import { ability, getCurrentOrg } from '@/auth/auth'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table'
+import { getInvites } from '@/http/get-invites'
+
+import RevokeInviteButton from './revoke-invite-button'
+
+export default async function Invites() {
+  const curretnOrg = getCurrentOrg()
+  const permissions = await ability()
+
+  const { invites } = await getInvites(curretnOrg!)
+
+  return (
+    <div className="space-y-4">
+      {permissions?.can('create', 'Invite') && (
+        <Card>
+          <CardHeader>
+            <CardTitle>Invite member</CardTitle>
+          </CardHeader>
+          <CardContent></CardContent>
+        </Card>
+      )}
+
+      <div className="rounded border">
+        <Table>
+          <TableBody>
+            {invites.map((invite) => (
+              <TableRow key={invite.id}>
+                <TableCell className="py-2.5">
+                  <span className="text-muted-foreground">{invite.email}</span>
+                </TableCell>
+
+                <TableCell className="py-2.5 font-medium">
+                  {invite.role}
+                </TableCell>
+
+                <TableCell className="py-2.5">
+                  <div className="flex justify-end">
+                    {permissions?.can('delete', 'Invite') && (
+                      <RevokeInviteButton inviteId={invite.id} />
+                    )}
+                  </div>
+                </TableCell>
+              </TableRow>
+            ))}
+
+            {invites.length === 0 && (
+              <TableRow>
+                <TableCell className="py-4 text-center text-muted-foreground">
+                  No invites found
+                </TableCell>
+              </TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </div>
+    </div>
+  )
 }
