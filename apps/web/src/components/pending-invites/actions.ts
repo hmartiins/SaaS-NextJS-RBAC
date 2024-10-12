69582a9f16af59e070a9b31cf606c54dbc410127
hmartiins <hmartins224@gmail.com>
2024-10-11 21:46:25 -0300

    feat: create pending invites setup

---
+++ apps/web/src/components/pending-invites/actions.ts
@@ -0,0 +1,15 @@
+'use server'
+
+import { revalidateTag } from 'next/cache'
+
+import { acceptInvite } from '@/http/accept-invite'
+
+export async function acceptInviteAction(inviteId: string) {
+  await acceptInvite(inviteId)
+
+  revalidateTag('organizations')
+}
+
+export async function rejectInviteAction(inviteId: string) {
+  await rejectInviteAction(inviteId)
+}
