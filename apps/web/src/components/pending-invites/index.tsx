cebd85c3a58d5c820540642c04c2b4a6618725ee
hmartiins <hmartins224@gmail.com>
2024-10-11 21:48:41 -0300

    fix: popover trigger in peding invites

--- apps/web/src/components/pending-invites/index.tsx
+++ apps/web/src/components/pending-invites/index.tsx
@@ -38,58 +38,58 @@ export function PendingInvites() {
 
   return (
     <Popover open={isOpen} onOpenChange={setIsOpen}>
-      <PopoverTrigger>
+      <PopoverTrigger asChild>
         <Button size={'icon'} variant={'ghost'}>
           <UserPlus2 className="size-4" />
           <span className="sr-only">Pending invites</span>
         </Button>
-
-        <PopoverContent className="w-80 space-y-2">
-          <span className="block text-sm font-medium">
-            Pending invites ({data?.invites.length ?? 0})
-          </span>
-
-          {data?.invites.length === 0 && (
-            <p className="text-sm text-muted-foreground">No invites found</p>
-          )}
-
-          {data?.invites.map((invite) => (
-            <div className="space-y-2">
-              <p className="text-sm leading-relaxed text-muted-foreground">
-                <span className="font-medium text-foreground">
-                  {invite.author?.name ?? 'Someone'}
-                </span>{' '}
-                invite you to join{' '}
-                <span className="font-medium text-foreground">
-                  {invite.organization.name}
-                </span>{' '}
-                <span>{dayjs(invite.createdAt).fromNow()}</span>
-              </p>
-
-              <div className="flex gap-1">
-                <Button
-                  size={'xs'}
-                  variant={'outline'}
-                  onClick={() => handleAcceptInvite(invite.id)}
-                >
-                  <Check className="mr-1.5 size-3" />
-                  Accept
-                </Button>
-
-                <Button
-                  size={'xs'}
-                  variant={'ghost'}
-                  className="text-muted-foreground"
-                  onClick={() => handleRejectInvite(invite.id)}
-                >
-                  <X className="mr-1.5 size-3" />
-                  Revoke
-                </Button>
-              </div>
-            </div>
-          ))}
-        </PopoverContent>
       </PopoverTrigger>
+
+      <PopoverContent className="w-80 space-y-2">
+        <span className="block text-sm font-medium">
+          Pending invites ({data?.invites.length ?? 0})
+        </span>
+
+        {data?.invites.length === 0 && (
+          <p className="text-sm text-muted-foreground">No invites found</p>
+        )}
+
+        {data?.invites.map((invite) => (
+          <div className="space-y-2">
+            <p className="text-sm leading-relaxed text-muted-foreground">
+              <span className="font-medium text-foreground">
+                {invite.author?.name ?? 'Someone'}
+              </span>{' '}
+              invite you to join{' '}
+              <span className="font-medium text-foreground">
+                {invite.organization.name}
+              </span>{' '}
+              <span>{dayjs(invite.createdAt).fromNow()}</span>
+            </p>
+
+            <div className="flex gap-1">
+              <Button
+                size={'xs'}
+                variant={'outline'}
+                onClick={() => handleAcceptInvite(invite.id)}
+              >
+                <Check className="mr-1.5 size-3" />
+                Accept
+              </Button>
+
+              <Button
+                size={'xs'}
+                variant={'ghost'}
+                className="text-muted-foreground"
+                onClick={() => handleRejectInvite(invite.id)}
+              >
+                <X className="mr-1.5 size-3" />
+                Revoke
+              </Button>
+            </div>
+          </div>
+        ))}
+      </PopoverContent>
     </Popover>
   )
 }
