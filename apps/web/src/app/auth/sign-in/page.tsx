091b27122f7f11a3e521089f415980c30561fd9b
hmartiins <hmartins224@gmail.com>
2024-10-17 19:55:19 -0300

    refactor(web): update sign-in page to include error generation button

--- apps/web/src/app/auth/sign-in/page.tsx
+++ apps/web/src/app/auth/sign-in/page.tsx
@@ -1,5 +1,12 @@
+import { Button } from '@/components/ui/button'
+
 import { SignInForm } from './sign-in-form'
 
 export default function SignInPage() {
-  return <SignInForm />
+  return (
+    <>
+      <SignInForm />
+      <Button onClick={() => console.error('Erro datadog!')}>Gerar Erro</Button>
+    </>
+  )
 }
