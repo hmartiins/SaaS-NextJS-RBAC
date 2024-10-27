eb5a9c6f45c8d73be123183be173c700dac07332
hmartiins <hmartins224@gmail.com>
2024-10-26 23:12:51 -0300

    feat(web): add dialog component and dialog confirmation in revoke invite button

--- apps/web/src/app/(app)/org/[slug]/members/revoke-invite-button.tsx
+++ apps/web/src/app/(app)/org/[slug]/members/revoke-invite-button.tsx
@@ -1,6 +1,14 @@
 import { XOctagon } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
 
 import { revokeInviteAction } from './actions'
 
@@ -12,11 +20,33 @@ export default function RevokeInviteButton({
   inviteId,
 }: RevokeInviteButtonProps) {
   return (
-    <form action={revokeInviteAction.bind(null, inviteId)}>
-      <Button variant="destructive" size={'sm'} type="submit">
-        <XOctagon className="mr-2 size-4" />
-        Revoke invite
-      </Button>
-    </form>
+    <Dialog>
+      <DialogTrigger>
+        <Button variant="destructive" size={'sm'} type="submit">
+          <XOctagon className="mr-2 size-4" />
+          Revoke invite
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Are you absolutely sure?</DialogTitle>
+          <DialogDescription>
+            This action cannot be undone. This will permanently delete this
+            account and remove your data from our servers.
+            <form action={revokeInviteAction.bind(null, inviteId)}>
+              <Button
+                variant="destructive"
+                size={'sm'}
+                type="submit"
+                className="mt-4"
+              >
+                <XOctagon className="mr-2 size-4" />
+                Revoke invite
+              </Button>
+            </form>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   )
 }
